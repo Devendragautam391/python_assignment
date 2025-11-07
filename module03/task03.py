7c7317e82ed0da47061e1695dd4e867cd161086d
devengautam43 <devengautam43@gmail.com>
2025-11-07 22:24:51 +0200

    Finished module03 task03

--- module03/task03.py
+++ module03/task03.py
@@ -1,16 +1,16 @@
 g=input("Enter your gender (male/female): ").strip().lower()
-h=float(input("Enter your hemoglobin level:")
-if gende =="female":
-    if hemoglobin < 117:
+h=float(input("Enter your hemoglobin level:"))
+if g=="female":
+    if h<117:
         print("Your hemoglobin level is LOW.")
-    elif hemoglobin <= 155:
+    elif h<=155:
         print("Your hemoglobin level is NORMAL.")
     else:
         print("Your hemoglobin level is HIGH.")
-elif gender == "male":
-    if hemoglobin < 134:
+elif g=="male":
+    if h<134:
         print("Your hemoglobin level is LOW.")
-    elif hemoglobin <= 167:
+    elif h<=167:
         print("Your hemoglobin level is NORMAL.")
     else:
         print("Your hemoglobin level is HIGH.")
