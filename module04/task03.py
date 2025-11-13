3418d77c2c2ecc300a897d4ba86cdd3dc533ee93
devengautam43 <devengautam43@gmail.com>
2025-11-13 14:01:25 +0200

    Finished module04 task03

--- module04/task03.py
+++ module04/task03.py
@@ -1,14 +1,21 @@
-numbers=[]
-while True:
-    user_input=input("Enter a number (or press Enter to stop): ")
-    if user_input=="":
-        break
-    number = float(user_input)
-    numbers.append(number)
-if numbers:
-    smallest = min(numbers)
-    largest = max(numbers)
-    print(f"\nSmallest number: {smallest}")
-    print(f"Largest number: {largest}")
+number = input("Enter a number (or press Enter to stop): ")
+if number == "":
+    print("No numbers were entered.")
 else:
-    print("\nNo numbers were entered.")
+    smallest = largest = float(number)
+    while True:
+        number = input("Enter another number (or press Enter to stop): ")
+        if number == "":
+            break
+        try:
+            num = float(number)
+        except ValueError:
+            print("Invalid input, please enter a valid number.")
+            continue
+        if num < smallest:
+            smallest = num
+        elif num > largest:
+            largest = num
+    print("Smallest number:", smallest)
+    print("Largest number:", largest)
+
