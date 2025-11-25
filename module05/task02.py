6d633988cc98cda7a9f2848a12924110bc5540fd
devengautam43 <devengautam43@gmail.com>
2025-11-25 23:16:04 +0200

    Finished module05 task02

---
+++ module05/task02.py
@@ -0,0 +1,14 @@
+numbers = []
+while True:
+    user_input = input("Enter a number (or press Enter to quit): ")
+    if user_input == "":
+        break
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+numbers.sort(reverse=True)
+print("The five greatest numbers are:")
+for n in numbers[:5]:
+    print(n)
