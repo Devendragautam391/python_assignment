8f0d2374ec971bb83853ec3f4344e6f4c613913b
devengautam43 <devengautam43@gmail.com>
2025-11-28 05:15:38 +0200

    finished module02 task06

--- module02/task06.py
+++ module02/task06.py
@@ -1,18 +1,11 @@
-
 import random
+value1 = random.randint(0,9)
+value2 = random.randint(0,9)
+value3 = random.randint(0,9)
+print(f"{value1} {value2} {value3}")
 
-def generate_3_digit_code():
-    # each digit 0-9
-    digits = [str(random.randint(0,9)) for _ in range(3)]
-    return "".join(digits)
-
-def generate_4_digit_code():
-    # each digit 1-6
-    digits = [str(random.randint(1, 6)) for _ in range(4)]
-    return "".join(digits)
-
-if __name__ == "__main__":
-    code3 = generate_3_digit_code()
-    code4 = generate_4_digit_code()
-    print("3-digit code (0-9 each):", code3)
-    print("4-digit code (1-6 each):", code4)
+value4 = random.randint(1,6)
+value5 = random.randint(1,6)
+value6 = random.randint(1,6)
+value7 = random.randint(1,6)
+print(f"{value4} {value5} {value6} {value7}")
