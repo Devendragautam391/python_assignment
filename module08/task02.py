2bf7c86b9f379a2114bcb53cc74e7a53f5f06410
devengautam43 <devengautam43@gmail.com>
2025-12-16 23:08:31 +0200

    finished module08 task02

--- module08/task02.py
+++ module08/task02.py
@@ -8,10 +8,11 @@ class Car:
             self.current_speed = self.maximum_speed
         if self.current_speed < 0:
             self.current_speed = 0
-car = Car(max_speed = 142)
-car.accelerate(30)
-car.accelerate(70)
-car.accelerate(50)
-print("Current speed:", car.current_speed)
-car.accelerate(-200)
+car = Car(142)
+max_speed = int(input("Enter maximum speed: "))
+n = int(input("How many speed changes? "))
+for i in range(n):
+    change = int(input(f"Enter speed change #{i+1}: "))
+    car.accelerate(change)
+    print("Current speed:", car.current_speed)
 print("Final speed:", car.current_speed)
